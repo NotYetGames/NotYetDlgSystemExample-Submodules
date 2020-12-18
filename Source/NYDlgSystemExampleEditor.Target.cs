dc2ca19b446fb6a1ccbcbb79ba48179199903e40
Daniel Butum <danibutum@gmail.com>
2020-12-18 20:29:37 +0200

    Update plugin

--- Source/NYDlgSystemExampleEditor.Target.cs
+++ Source/NYDlgSystemExampleEditor.Target.cs
@@ -9,6 +9,9 @@ public class NYDlgSystemExampleEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		// bUseUnityBuild = false;
+		// bUseSharedPCHs = false;
+		// bUsePCHFiles = false;
 
 		ExtraModuleNames.AddRange( new string[] { "NYDlgSystemExample" } );
 	}
